52f64bd793d27fd9a864e057d286a874d2d685c5
Pedro Stanzani <pedrohstanzani@gmail.com>
2022-12-10 17:31:00 -0300

    Add mock data and get all books route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
 
+let books = [
+  {
+    id: 1,
+    title: 'The Catcher in the Rye',
+    author: 'J.D. Salinger',
+    year: 1951
+  },
+  {
+    id: 2,
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    year: 1960
+  },
+
+];
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+})
+
+app.get('/api/books', (req, res) => {
+  res.json(books);
 })
 
 const PORT = 3001;
