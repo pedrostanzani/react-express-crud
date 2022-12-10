87e2300a49be9e13aefbf0acaef5908708713704
Pedro Stanzani <pedrohstanzani@gmail.com>
2022-12-10 19:20:32 -0300

    Add delete route

--- server/index.js
+++ server/index.js
@@ -33,6 +33,16 @@ app.get('/api/books/:id', (req, res) => {
   else { res.status(404).end(); }
 })
 
+app.delete('/api/books/:id', (req, res) => {
+  // Excerpt from: https://fullstackopen.com/en/part3/node_js_and_express#deleting-resources
+  // There's no consensus on what status code should be returned to a DELETE request if the resource does not exist. 
+  // Really, the only two options are 204 and 404. 
+  // For the sake of simplicity our application will respond with 204 in both cases.
+  const id = Number(req.params.id);
+  books = books.filter(book => book.id !== id)
+  res.status(204).end()
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
