201e3f6640a9f27ad0cdec2fdc14c2fc43457fa0
Pedro Stanzani <pedrohstanzani@gmail.com>
2022-12-10 19:17:25 -0300

    Add get individual resource route

--- server/index.js
+++ server/index.js
@@ -25,6 +25,14 @@ app.get('/api/books', (req, res) => {
   res.json(books);
 })
 
+app.get('/api/books/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const book = books.find(book => book.id === id);
+
+  if (book) { res.json(book); } 
+  else { res.status(404).end(); }
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
