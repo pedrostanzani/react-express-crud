c375f28d07b480df3ac3fa1b6cbcda39994d5d54
Pedro Stanzani <pedrohstanzani@gmail.com>
2022-12-10 17:16:31 -0300

    Add index (hello world) server route

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log("Hello, server!");
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
